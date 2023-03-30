2215b740b21a8f4897cec2164631082e69706dbe
Brandon Guerrero <brandonguerrero@Brandons-MacBook-Air.local>
2023-03-30 13:47:07 -0700

    added index.js

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# read-me-generator
+# Tesla UI
+  [![MIT license](https://img.shields.io/badge/License-MIT-blue.svg)](https://lbesson.mit-license.org/)
+  ## Table of Contents
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Licenses](#licenses)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  * [Credits](#credits)
+  ## Description
+  Design a new UI for the latest vehicle update
+  ## Installation
+  install the latest inquirer by running this commandnpm i inquirer@8.2.4
+  ## Usage
+  run node index.js in the terminal
+  ## Licenses
+    This project is covered under the MIT license. To learn more about what this means, click the license button at the top.
+  ## Contributing
+  cloning the repo and adding ideas
+  ## Tests
+  Designing new UI features
+  ## Questions
+  Have questions about this project?  
+  GitHub: https://github.com/bg2398  
+  Email: brandon@github.com
+  ## Credits
+  Brandon Guerrero
---
+++ index.js
@@ -0,0 +1,163 @@
+// TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./generateMarkdown');
+// TODO: Create an array of questions for user input
+const questions = [
+ {
+    type: 'input',
+    name: 'name',
+    message: 'Hello! to start please provide your full name:',
+    validate: nameInput => {
+        if(nameInput) {
+            return true;
+        } else {
+            console.log('You forgot to enter your name');
+            return false;
+        }
+    }
+ },
+ {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username:',
+    validate: githubInput => {
+        if(githubInput) {
+        return true;
+        } else {
+            console.log('Please enter your Github username');
+            return false;
+        }
+    }
+ },
+ {
+    type: 'input',
+    name: 'email',
+    message: 'Enter yourt email',
+    validate: emailInput => {
+        if(emailInput) {
+            return true;
+        } else {
+            console.log('Please enter your email');
+            return false;
+        }
+    }
+ },
+ {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Every project must have a title. Please try again.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'description',
+    message: "Enter your project description here:",
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else {
+            console.log('It is essential to provide a description of your project. Not sure what to include? Head to the repo of this README generator and navigate to the section "Description: Questions to Consider" under the Guidelines header for some tips on writing a quality description.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'installation',
+    message: 'What are the instructions for installation?',
+    validate: installationInput => {
+        if (installationInput) {
+            return true;
+        } else {
+            console.log('Please provide instructions for installation to ensure users have the proper software to run your program!');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'usage',
+    message: 'Instructions for usage:',
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log('Providing instructions for usage will help users properly navigate your project. Please try again.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'contributing',
+    message: 'How can others contribute to this project?',
+    validate: contributionInput => {
+        if (contributionInput) {
+            return true;
+        } else {
+            console.log('Please provide instructions on how others can contribute to your project.');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    name: 'tests',
+    message: 'Describe the tests written for your application and how to use them:',
+    validate: testsInput => {
+        if (testsInput) {
+            return true;
+        } else {
+            console.log('Please provide instructions on how others can contribute to your project.');
+            return false;
+        }
+    }
+},
+{
+    type: 'confirm',
+    name: 'confirmLicenses',
+    message: 'Would you like to include a license?',
+    default: false
+},
+{
+    type: 'list',
+    name: 'licenses',
+    message: 'What license would you like to include?',
+    choices: ['MIT', 'GPL', 'CC--0'],
+    when: ({ confirmLicenses }) => {
+        if (confirmLicenses) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+},
+];
+
+// TODO: Create a function to write README file
+const writeToFile = data => {
+    return new Promise((resolve, reject) => {
+        // make a readme file
+        fs.writeFile('./README.md', data, err => {
+            // if there's an error, reject the Promise and send the error to .catch() method
+            if (err) {
+                reject (err);
+                // return out of the function here to make sure the Promise doesn't continut to execute the resolve() function
+                return;
+            }
+            // if everything went well, resolve the Promise and send the successful data to the .then() method
+            resolve({
+                ok: true,
+                message: console.log('Success! Navigate to the "dist" folder to see your README!')
+            });
+        })
+    })
+}
